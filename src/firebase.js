3fb46618a6853749fc1ff02e2c1c57cd9b77546d
mdariful.islam <mdariful.islam@bracits.com>
2022-08-06 14:31:03 +0600

    Arif | develop | add firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC1NXphnVM_ojU3lvplECeVlGJ2NdB-p3g",
+  authDomain: "clone-da669.firebaseapp.com",
+  projectId: "clone-da669",
+  storageBucket: "clone-da669.appspot.com",
+  messagingSenderId: "1027267718421",
+  appId: "1:1027267718421:web:451fb943f4f0b4cde28d1c",
+  measurementId: "G-5CYXGMQVV5",
+};
