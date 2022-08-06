cb99906d0fca1d0b00e8c557c337269c39fa434c
mdariful.islam <mdariful.islam@bracits.com>
2022-08-06 23:59:10 +0600

    Arif | develop | checkout page styling

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -6,7 +6,21 @@ function Checkout() {
   return (
     <>
       <Header />
-      <div className="checkout"></div>
+      <div className="checkout">
+        <div className="checkout__left">
+          <img
+            className="checkout__add"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img18/Coupons/ART/Diwali-18/stripe_pc._CB483582784_.jpg"
+            alt="image"
+          />
+          <div>
+            <h2 className="checkout__title">Your shopping basket</h2>
+          </div>
+        </div>
+        <div className="checkout__right">
+          <h2>subtotal will go here</h2>
+        </div>
+      </div>
     </>
   );
 }
