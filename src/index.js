854f1c166ad405376276983933338fd1234b01e0
mdariful.islam <mdariful.islam@bracits.com>
2022-08-07 14:15:30 +0600

    Arif | develop | remove state provider

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import { StateProvider } from "./StateProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
+    <App />
   </React.StrictMode>
 );
 
